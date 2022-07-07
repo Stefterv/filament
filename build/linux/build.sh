8c3a2a0f75b6554b06eb780adc03c6b9751e46e0
Stef Tervelde <stef@steftervelde.nl>
2022-07-07 15:07:59 +0200

    Skip cleaning dir

--- build/linux/build.sh
+++ build/linux/build.sh
@@ -36,4 +36,4 @@ source `dirname $0`/ci-common.sh
 source `dirname $0`/../common/build-common.sh
 
 pushd `dirname $0`/../.. > /dev/null
-./build.sh -c $RUN_TESTS $GENERATE_ARCHIVES $BUILD_DEBUG $BUILD_RELEASE
+./build.sh $RUN_TESTS $GENERATE_ARCHIVES $BUILD_DEBUG $BUILD_RELEASE
